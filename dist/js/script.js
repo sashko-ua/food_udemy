634e56f829be4e3e9487f126a8deaf412191c676
Sashko <sashkoace@gmail.com>
2020-08-21 11:55:07 +0300

    added timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -94,6 +94,7 @@
 /***/ (function(module, exports) {
 
 window.addEventListener('DOMContentLoaded', () => {
+  // __________TABS__________
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParrent = document.querySelector('.tabheader__items');
@@ -130,7 +131,56 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); // __________TIMER__________
+
+  const deadLine = '2020-10-21';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - new Date(),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          seconds = Math.floor(t / 1000 % 60),
+          minutes = Math.floor(t / 1000 / 60 % 60),
+          hours = Math.floor(t / (1000 * 60 * 60) % 24);
+    return {
+      t,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.t <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadLine);
 });
 
 /***/ })
